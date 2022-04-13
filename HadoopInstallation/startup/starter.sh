48eaae7447248f30c9ef72ef06e27f9121075a72
Ganesh Kumar <ganesh.kumar@troondx.com>
2022-04-13 16:52:12 +0530

    adding start and stop scripts

---
+++ HadoopInstallation/startup/starter.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo service ssh start
+sudo service mysql start
+start-dfs.sh
+start-yarn.sh
+
---
+++ HadoopInstallation/stopservice/stopall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+stop-dfs.sh
+stop-yarn.sh
+sudo service ssh stop
+sudo service mysql stop
